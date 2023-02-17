62018a4090ec2ac2a9d6ac21378e56bf94cba4b5
katakuri-it <kutmanov_b@iuca.kg>
2023-02-17 22:53:37 +0300

    Created page for forms.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,3 +12,20 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_details.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
